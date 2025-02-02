0f8e0bb3a2823f282bbb984f75c3ecfbf82ee1a4
abd <61125500+AbdRamdani@users.noreply.github.com>
2025-02-02 17:04:38 +0100

    Update synchronisation-s3.py

--- deploy_with_docker/synchronisation-s3.py
+++ deploy_with_docker/synchronisation-s3.py
@@ -12,38 +12,37 @@ def init_s3_minio(endpoint_url, access_key_minio, secret_key_minio):
 
 def synchronisationS3(s3,folder_path,bucket_name):
 
-    # obtenir la liste des fichiers qui existe dans le bucket_name
+    # Get the list of files in the S3 bucket
     response = s3.list_objects_v2(Bucket=bucket_name)
     bucket_files = {obj['Key']: obj for obj in response.get('Contents', [])}
 
-    # la liste des fichiers locaux (sur le dossier folder_path)
-        # le path est aussi sauvgarder \folder_path\filename pour verifier avec les fichier sur les buckets
-        # en respectent la notion des clés qui representent l'arboresence 
+    # Get the list of local files in the specified directory
+    # The path is stored as full_path to compare with bucket keys
+    # while respecting the key structure that simulates a directory hierarchy. 
     onlyfiles = []
     for root, _, files in os.walk(folder_path):
         for file in files:
             full_path = os.path.join(root, file)
             onlyfiles.append(full_path)
-    
-    # 1er et 3eme cas dans la meme boucle (verfier si le fichier local existe sur le bucket et si il est a jour)
+    # Check if the local file exists in the bucket and if it's up to date
     for local_file in onlyfiles:
-        #car le premier / est retirer car les clés des bucket ne contenant pas un / en premier (sinon il n'arrivera pas a comparer entre les 2 --> n'existe pas toujours)
+        # Remove leading slashes from the local file path for comparison
         if local_file.lstrip('/') not in bucket_files:
-            print(f"cas 1 : local file: {local_file} n'existe pas dans le bucket {bucket_name} --> uploading")
+            print(f"local file {local_file} does not exist in bucket {bucket_name} --> uploading")
             s3.upload_file(local_file, bucket_name, local_file)
         else:
-            # cas 3 : vérifier si la version locale est plus récente que celle dans le bucket
+            # Check if the local file is more recent than the one in the bucket
             local_modified_time = os.path.getmtime(local_file)
             bucket_modified_time = bucket_files[local_file.lstrip('/')]['LastModified'].timestamp()
             if local_modified_time > bucket_modified_time:
-                print(f"cas 3 : Local file {local_file} est plus recent que celui dans le bucket {bucket_name} --> updating")
+                print(f"local file {local_file} is more recent than the file in bucket {bucket_name} --> updating")
                 s3.upload_file(local_file, bucket_name, local_file)
  
-    #2eme cas : supprimer les fichier qui sont dans le bucket mais qui ne sont pas dans le local 
-    local_files = set(f.lstrip('/') for f in onlyfiles) #retirer le / dans les debut des noms des fichiers pour comparer 
+    # Delete files in the bucket that no longer exist locally 
+    local_files = set(f.lstrip('/') for f in onlyfiles) # Remove leading slashes for comparison
     for bucket_file in bucket_files:
         if bucket_file not in local_files and bucket_file.startswith(folder_path.lstrip('/')):
-            print(f"cas 2 : le fichier {bucket_file} n'existe pas en local --> deleting")
+            print(f"the file {bucket_file} does not exist locally --> deleting")
             s3.delete_object(Bucket=bucket_name, Key=bucket_file)
 
 
@@ -54,4 +53,4 @@ if __name__ == "__main__":
     bucket_name = sys.argv[2]
     s3 = init_s3_minio('http://172.17.0.2:9000', 'minio', 'miniokey')
     synchronisationS3(s3,folder_path,bucket_name)
-    print('------- fin de programme -----------')
+    print('------- End of program -----------')
